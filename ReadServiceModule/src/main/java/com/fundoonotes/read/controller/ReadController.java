e92a235f77351ff081ee97cd933d94ec6e2486f6
bridgeit <pranitkokne2018@gmail.com>
2018-07-17 16:10:04 +0530

    read service with global exception handling

--- ReadServiceModule/src/main/java/com/fundoonotes/read/controller/ReadController.java
+++ ReadServiceModule/src/main/java/com/fundoonotes/read/controller/ReadController.java
@@ -2,12 +2,16 @@ package com.fundoonotes.read.controller;
 
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import com.fundoonotes.read.repository.NoteRepository;
 import com.fundoonotes.read.repository.UserRepository;
+import com.fundoonotes.read.util.NoteNotFoundException;
+import com.fundoonotes.read.util.UserNotFoundException;
 
 @RestController
 public class ReadController {
@@ -17,18 +21,27 @@ public class ReadController {
 
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	@RequestMapping(value = "todo/{index}/{type}/{id}", method = RequestMethod.GET)
-	public Map<String, Object> getNoteById(@PathVariable Map<String, String> pathValues) {
+	public ResponseEntity<Map<String, Object>> getNoteById(@PathVariable Map<String, String> pathValues) {
 		String index = pathValues.get("index");
 		String type = pathValues.get("type");
 		String id = pathValues.get("id");
-		return noteRepository.getNoteById(index, type, id);
+		Map<String, Object> note = noteRepository.getNoteById(index, type, id);
+		if (note == null) {
+			throw new NoteNotFoundException("Note is not present");
+		}
+		return new ResponseEntity<>(note, HttpStatus.OK);
 	}
 
 	@RequestMapping(value = "/fundoonotes/{key}/{hashKey}", method = RequestMethod.GET)
-	public Object getUserById(@PathVariable("key") String key,@PathVariable("hashKey") String hashKey) {
-		return userRepository.getUserById(key, hashKey);
+	public ResponseEntity<Object> getUserById(@PathVariable("key") String key,
+			@PathVariable("hashKey") String hashKey) {
+		Object user = userRepository.getUserById(key, hashKey);
+		if (user == null) {
+			throw new UserNotFoundException("User is not present");
+		}
+		return new ResponseEntity<Object>(user, HttpStatus.OK);
 	}
 
 }
